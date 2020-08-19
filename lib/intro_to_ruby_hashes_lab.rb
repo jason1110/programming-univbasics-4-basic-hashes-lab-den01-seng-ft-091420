4575477cca533fd923d40d800967ab2395d1eaa1
Jason Easterly <jason.easterly11@gmail.com>
2020-08-19 21:27:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
-  # return an empty hash
+  empty_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  single_hash = {
+    fruit: "orange"
+  }
 end
 
 def pioneer
